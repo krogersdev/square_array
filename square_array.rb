658954713f77c1b405eaf3e119617712f2430f8f
Kevin <kevinarogers1@gmail.com>
2021-02-08 18:21:41 -0500

    commit tree ad9b2ea6ee69b94e926069376ad584ab7427371e

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
-def square_array(array)
-  # your code here
+  def square_array(array)
+    new_array = []
+      array.each do |number|
+      new_array << (number** 2)
+    end
+    new_array
 end
